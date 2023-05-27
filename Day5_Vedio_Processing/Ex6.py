730890d3e452024a05cf4559e37d500478c4722c
AJAX <22mapbs019@ddu.ac.in>
2023-05-27 18:55:08 +0530

    Day5 Vedio Processing

--- Day5_Vedio_Processing/Ex6.py
+++ Day5_Vedio_Processing/Ex6.py
@@ -1,1 +1,25 @@
 #rotet Live  Vedio 
+import cv2
+
+video = cv2.VideoCapture(0)
+size = (int(video.get(3)), int(video.get(4)))
+result = cv2.VideoWriter('Ex3Output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, size)
+
+while video.isOpened():
+    ret, frame = video.read()
+
+    # Rotate the frame by 90 degrees clockwise
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+    result.write(frame)
+
+    if ret:
+        cv2.imshow('Frame', frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+    else:
+        break
+
+video.release()
+cv2.destroyAllWindows()
--- Day5_Vedio_Processing/Ex7.py
+++ Day5_Vedio_Processing/Ex7.py
@@ -1,1 +1,22 @@
-#Extract Audio from Vedio
+#Extract Audio from Vedio   
+import cv2
+import subprocess
+
+video = cv2.VideoCapture('Demo.mp4')
+while video.isOpened():
+    ret, frame = video.read()
+    if ret:
+        cv2.imshow('Frame', frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+    else:
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+# Extract audio from the video file
+output_audio_file = 'audio.wav'
+audio_cmd = ['ffmpeg', '-y', '-i', 'Demo.mp4', '-vn', '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '2', output_audio_file]
+subprocess.call(audio_cmd)
